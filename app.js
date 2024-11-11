e810ab05cf1a0e005fa717b165b93b8cf8b06ad7
mahalabsi <mahatalabsi@gmail.com>
2024-11-11 11:46:10 +0300

    4

--- app.js
+++ app.js
@@ -161,6 +161,10 @@ the past to those born most recently).
 */
 
 // Complete the exercise in the space below:
+// let sortedByBirthYear = inventors.sort((inventor.year, inventor.passed => ) => {
+
+// })
+
 let sortedByBirthYear = inventors.slice().sort((a, b) => a.year - b.year)
 
 // Check your work:
@@ -180,6 +184,30 @@ console.log('Exercise 3 correct result: ', [
   { first: 'Katherine', last: 'Blodgett', year: 1898, passed: 1979 }
 ])
 
-// let sortedByBirthYear = inventors.sort((inventor.year, inventor.passed => ) => {
+/* 
+Exercise 4: Array.prototype.find()
 
-// })
+Use Array.prototype.find() to identify the inventor with the first name 'Ada' 
+from an array of inventor objects
+
+- You have an array of objects, each representing an inventor with various 
+  properties including their first name.
+- Utilize the Array.prototype.find() method to locate the object for the 
+  inventor with the first name 'Ada'.
+- Assign the found inventor object to the variable inventorNamedAda
+*/
+
+let inventorNamedAda = inventors.find((inventor) => {
+  return inventor.first === 'Ada'
+})
+
+// Complete the exercise in the space below:
+
+// Check your work:
+console.log('Exercise 4 my result: ', inventorNamedAda)
+console.log('Exercise 4 correct result: ', {
+  first: 'Ada',
+  last: 'Lovelace',
+  year: 1815,
+  passed: 1852
+})
