7a9a4f11609cb17486ad2244d468ef7bccd17b21
mahalabsi <mahatalabsi@gmail.com>
2024-11-11 12:22:02 +0300

    8,9

--- app.js
+++ app.js
@@ -319,3 +319,41 @@ let isEveryone19OrOlder = devs.every((all) => {
 // Check your work:
 console.log('Exercise 7 my result: ', isEveryone19OrOlder)
 console.log('Exercise 7 correct result: ', false)
+
+/*
+Exercise 8: Array.prototype.find()
+
+Use Array.prototype.find() to identify and retrieve the comment object with 
+a specific ID 823423 from an array of comment objects.
+
+- Assign the found comment object to the variable 'commentById'.
+*/
+
+let commentById = comments.find((ids) => {
+  return ids.id === 823423
+})
+
+// Complete the exercise in the space below:
+
+// Check your work:
+console.log('Exercise 8 my result: ', commentById)
+console.log('Exercise 8 correct result: ', { text: 'Super good', id: 823423 })
+
+/*
+Exercise 9: Array.prototype.findIndex()
+
+Determine the index of the comment that has a specific ID 123523 in an array 
+of comment objects.
+
+- Store the index in the variable 'idx'.
+*/
+
+let idx = comments.findIndex((comment) => {
+  return comment.id === 123523
+})
+
+// Complete the exercise in the space below:
+
+// Check your work:
+console.log('Exercise 9 my result: ', idx)
+console.log('Exercise 9 correct result: ', 3)
